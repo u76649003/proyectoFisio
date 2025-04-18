4372f0ce618e12116f43c5c37413f0755afdf377
u76649003 <justojgd@gmail.com>
2025-04-18 14:44:20 +0200

    Update SwaggerConfig.java
    anadir liberia

--- backend/src/main/java/com/proyectofisio/infrastructure/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/com/proyectofisio/infrastructure/config/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.proyectofisio.infrastructure.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
